3b04f653154d3bbcba0be76430bc3fee3937a8c6
jain.anoushka24 <jain.anoushka3@gmail.com>
2022-05-03 13:51:18 +0200

    cleaned the script

--- mainScript_AJ.py
+++ mainScript_AJ.py
@@ -4,7 +4,7 @@ Created on Thu Apr  7 13:47:19 2022
 
 @author: jain
 """
-fPath = r'D:\SharedEcephys\FromDimos\20220208_DKHMS_60pMEA10030_re_n1'
+fPath = r'Y:\invivo_ephys\SharedEphys\FromFermino\Kilosort2_2021-03-13_180605'
 # fPath = r'Z:\invivo_ephys\SharedEphys\FromGia\recording1'
 # fPath = r'D:\test'
 
@@ -62,10 +62,12 @@ eng.pC_getSyncMetric(fPath)
 
 #%% run noise Classifier and save output
 df = pd.read_csv('metrics.csv')
-percent_missing = df.isnull().sum() * 100 / len(df)
+
 # baseParams.useNoiseMetrics contains all the metrics of interest. 
 # make sure this includes cluster_id
-baseParams.useNoiseMetrics.append('cluster_id')
+
+
+
 baseParams.useNoiseMetrics = list(set(baseParams.useNoiseMetrics))
 
 frames = merge_frames([fPath], baseParams.useNoiseMetrics) # get metrics from recording
@@ -74,13 +76,46 @@ frames = merge_frames([fPath], baseParams.useNoiseMetrics) # get metrics from re
 # when manual labelling is in phy, the changes are reflected in cluster_group.csv 
 gTruth = merge_frames([fPath], ['group']) 
 
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from preprocessing import create_preprocessing_pipeline
+
+frame = frames[0][0]
+def preprocess_dataframe(frame):
+    # removing columns with high precentage of nans 
+    if 'epoch_name' in frame.columns:
+        frame.drop(['epoch_name'],axis = 1,inplace=True)
+    if 'Var1' in frame.columns:
+        frame.drop(['Var1'],axis = 1,inplace=True)
+        
+    percent_missing = frame.isnull().sum() * 100 / len(frame)
+    p = percent_missing.where(percent_missing > 80.0).dropna()
+    print("columns dropped : " ,  p)
+    
+    #interpolation to handle missing values
+    trans_frame = frame.interpolate(axis = 1, limit =5, limit_direction= 'both' ) 
+    percent_missing = trans_frame.isnull().sum() * 100 / len(trans_frame)
+    # imputing the dataframe with    technique
+    
+    
+    
+    preprocessing_pipeline = create_preprocessing_pipeline()
+    processed_frame = preprocessing_pipeline.fit_transform(trans_frame)
+    return processed_frame
+
+frames[0][0] = preprocess_dataframe(frames[0][0])
+
 df_auc = plot_roc_curve(fPath,frames,gTruth) # get AUC values for each metric
 
-rslt_df = df_auc.loc[(df_auc.roc_auc > 0.69) | (df_auc.roc_auc < 0.31)] # get metric with high AUC values 
+rslt_df = df_auc.loc[(df_auc.roc_auc > 0.59) | (df_auc.roc_auc < 0.41)] # get metric with high AUC values 
+
 rocMetrics = list(rslt_df.metric)
 print(set(rocMetrics).intersection(set(baseParams.useNoiseMetrics)))
 use_rocMetrics = set(rocMetrics).intersection(set(baseParams.useNoiseMetrics))
 
+baseParams.useNoiseMetrics.append('cluster_id')
 run_predictor(frames[0][0]) # test classifier
 
 mapping = {False: 'neural', True: 'noise'}
